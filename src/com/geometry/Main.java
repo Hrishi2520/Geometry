2d91ab2b8c8addb4c3b0cfd52c69a00ee1925147
Hrishi2520 <81430547+Hrishi2520@users.noreply.github.com>
2021-12-29 20:35:19 +0530

    - add findPoint method in Graph.java - add todos for distanceBetween and removePoint methods.
    Signed-off-by: Hrishi2520 <81430547+Hrishi2520@users.noreply.github.com>

--- src/com/geometry/Main.java
+++ src/com/geometry/Main.java
@@ -1,17 +1,22 @@
 package com.geometry;
 
 import com.geometry.graph.Graph;
+import com.geometry.graph.Point;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        Graph newGraph = new Graph("Salary");
-        newGraph.addPoint(10, -15, "ketan");
-        newGraph.addPoint(10, -15, "ketan");
-        newGraph.addPoint(6, 25);
-        newGraph.addPoint(-4, -12);
-        newGraph.addPoint(-70, 100, "Bhavesh");
-        System.out.printf("%s points: %s\n", newGraph.getLabel(), newGraph.points());
+        // write your code here
+        Graph salary = new Graph("Salary");
+        salary.addPoint(10, -15, "ketan");
+        salary.addPoint(10, -15, "ketan");
+        salary.addPoint(6, 25);
+        Point point = salary.addPoint(-4, -12);
+        Point bhavesh = salary.addPoint(-70, 100, "Bhavesh");
+        System.out.printf("findPoint(%s): %s\n", bhavesh, salary.findPoint(bhavesh));
+        System.out.printf("findPoint(%.2f, %.2f): %s\n", -110D, 120D, salary.findPoint(-110D, 120D));
+        System.out.printf("findPoint(%s): %s\n", "ketan", salary.findPoint("ketan"));
+        System.out.printf("%s points: %s\n", salary.getLabel(), salary.getPoints());
+
     }
 }
--- src/com/geometry/graph/Graph.java
+++ src/com/geometry/graph/Graph.java
@@ -11,24 +11,54 @@ public class Graph {
     private static final Quadrant THIRD = new Quadrant("third", new Range(Integer.MIN_VALUE, 0), new Range(Integer.MIN_VALUE, 0));
     private static final Quadrant FOURTH = new Quadrant("fourth", new Range(0, Integer.MAX_VALUE), new Range(Integer.MIN_VALUE, 0));
     private String label;
-    private List<Point> points;
+    private final List<Point> points;
 
     public Graph(String label) {
         this.label = label;
         points = new LinkedList<>();
     }
+//    TODO: add method remove point which helps to remove a point from graph as well as the quadrant and point is provided as point p or x, y.
+//    TODO: add method distancebetweeen which helps to find distance between two points provided as p1, p2 or x1, y1 and x2, y2 or point p or x, y.
 
     public Point addPoint(double x, double y, String label) {
         Point newPoint = new Point(x, y, label);
         Quadrant container = findQuadrant(newPoint);
         if (container.checkIfUnique(newPoint)) {
             container.points.add(newPoint);
+            points.add(newPoint);
             System.out.printf("Successfully created %s in %s-Quadrant\n", newPoint, container);
             return newPoint;
         }
 
         System.out.printf("Adding %s failed\n", newPoint);
         return null;
+    }
+
+    public Point findPoint(double x, double y) {
+        for (Point p : points) {
+            if (p.getX() == x && p.getY() == y)
+                return p;
+        }
+
+        return null;
+    }
+
+    public Point findPoint(String label) {
+        for (Point p : points) {
+            if (p.getLabel().equals(label))
+                return p;
+        }
+
+        return null;
+    }
+
+    public Point findPoint(Point point) {
+        return points.contains(point) ? point : null;
+    }
+
+
+    public Point addPoint(double x, double y) {
+        return addPoint(x, y, String.format("unnamed_%d", getPoints().size()));
     }
 
     private Quadrant findQuadrant(Point newPoint) {
@@ -48,16 +78,7 @@ public class Graph {
         this.label = label;
     }
 
-    public List<Point> points() {
-        List<Point> points = new LinkedList<>();
-        points.addAll(FIRST.points);
-        points.addAll(SECOND.points);
-        points.addAll(THIRD.points);
-        points.addAll(FOURTH.points);
+    public List<Point> getPoints() {
         return points;
-    }
-
-    public Point addPoint(double x, double y) {
-        return addPoint(x, y, String.format("unnamed_%d", points().size()));
     }
 }
