576419c46c935cad131ebc84367c5780a5696c5e
vishalz-cognologix <vishal.zambare@cognologix.com>
2021-12-29 19:09:27 +0530

    - Fix StackOverflow error at recursive call for Point.java ctor - Add checkIfUnique method in Quadrant.java - Replace add method call and foreach loop with addAll

--- src/com/geometry/graph/Quadrant.java
+++ src/com/geometry/graph/Quadrant.java
@@ -18,5 +18,23 @@ final class Quadrant {
         this.points = new LinkedList<>();
     }
 
+    public boolean checkIfUnique(Point point) {
+        for(Point p: points) {
+            if (p.getLabel().equals(point.getLabel())) {
+                System.out.printf("Found %s with same label as %s\n", point, p);
+                return false;
+            }
 
+            if (p.getX() == point.getX() && p.getY() == point.getY()) {
+                System.out.printf("Found %s with same co-ordinates as %s\n", point, p);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s", label);
+    }
 }
