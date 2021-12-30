6b71aca15021175e72f10c8d660fe257feda5274
Bhavesh Vikas Kadam <bhaukadam508@gmail.com>
2021-12-30 20:58:33 +0530

    Add origin point at initialization

--- src/com/geometry/Main.java
+++ src/com/geometry/Main.java
@@ -9,6 +9,7 @@ public class Main {
         // write your code here
         Graph friends = new Graph("friends");
         Point ketan = friends.addPoint(5, 0, "ketan");
+
         Point hrishi = friends.addPoint(0, 2, "hrishi");
         System.out.printf("Distance between %s and %s is %.2f units\n", ketan, hrishi, friends.distanceBetween(ketan, hrishi));
         friends.movePoint(hrishi.getX(), hrishi.getY(), 5, 33);
