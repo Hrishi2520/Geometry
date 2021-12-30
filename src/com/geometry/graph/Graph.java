fea74ed8ebcfe599254c8b74889090dbf819671c
Hrishi2520 <81430547+Hrishi2520@users.noreply.github.com>
2021-12-30 13:04:13 +0530

    Update Graph.java (#3)
    - Add pop overload methods
    - Add removePoint overload methods
    - Add distanceBetween overload methods
    - Restructure Graph.java
    
    Co-authored-by: Ketan Jitendra Shinde <ks27543926@gmail.com>

--- src/com/geometry/graph/Graph.java
+++ src/com/geometry/graph/Graph.java
@@ -17,8 +17,6 @@ public class Graph {
         this.label = label;
         points = new LinkedList<>();
     }
-//    TODO: add method remove point which helps to remove a point from graph as well as the quadrant and point is provided as point p or x, y.
-//    TODO: add method distancebetweeen which helps to find distance between two points provided as p1, p2 or x1, y1 and x2, y2 or point p or x, y.
 
     public Point addPoint(double x, double y, String label) {
         Point newPoint = new Point(x, y, label);
@@ -32,6 +30,19 @@ public class Graph {
 
         System.out.printf("Adding %s failed\n", newPoint);
         return null;
+    }
+
+    public Point addPoint(double x, double y) {
+        return addPoint(x, y, String.format("unnamed_%d", getPoints().size()));
+    }
+
+    private Quadrant findQuadrant(Point newPoint) {
+        Quadrant found;
+        if (FIRST.rangeX.inRange(newPoint.getX()) && FIRST.rangeY.inRange(newPoint.getY())) found = FIRST;
+        else if (SECOND.rangeX.inRange(newPoint.getX()) && SECOND.rangeY.inRange(newPoint.getY())) found = SECOND;
+        else if (THIRD.rangeX.inRange(newPoint.getX()) && THIRD.rangeY.inRange(newPoint.getY())) found = THIRD;
+        else found = FOURTH;
+        return found;
     }
 
     public Point findPoint(double x, double y) {
@@ -56,18 +67,62 @@ public class Graph {
         return points.contains(point) ? point : null;
     }
 
-
-    public Point addPoint(double x, double y) {
-        return addPoint(x, y, String.format("unnamed_%d", getPoints().size()));
+    public Point pop(String label) {
+        points.remove(findPoint(label));
+        return null;
     }
 
-    private Quadrant findQuadrant(Point newPoint) {
-        Quadrant found;
-        if (FIRST.rangeX.inRange(newPoint.getX()) && FIRST.rangeY.inRange(newPoint.getY())) found = FIRST;
-        else if (SECOND.rangeX.inRange(newPoint.getX()) && SECOND.rangeY.inRange(newPoint.getY())) found = SECOND;
-        else if (THIRD.rangeX.inRange(newPoint.getX()) && THIRD.rangeY.inRange(newPoint.getY())) found = THIRD;
-        else found = FOURTH;
-        return found;
+    public Point pop(double x, double y) {
+        points.remove(findPoint(x,y));
+        return null;
+    }
+
+    public Point pop(Point point) {
+        points.remove(findPoint(point));
+        return null;
+    }
+
+    public boolean removePoint(String label) {
+        return pop(label) != null;
+    }
+
+    public boolean removePoint(Point point) {
+        return pop(point) != null;
+    }
+
+    public boolean removePoint(double x, double y) {
+        return pop(x, y) != null;
+    }
+
+    public double distanceBetween(double x1, double y1, double x2, double y2) {
+        return Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2));
+    }
+
+    public double distanceBetween(Point p1, Point p2) {
+        return distanceBetween(p1.getX(), p1.getY(), p2.getX(), p2.getY());
+    }
+
+    public double distanceBetween(Point point) {
+        return distanceBetween(point.getX(), point.getY(), 0, 0);
+    }
+
+    public double distanceBetween(String label) {
+        Point found = findPoint(label);
+        if (found != null)
+            return distanceBetween(found.getX(), found.getY(), 0, 0);
+        return -1D;
+    }
+
+    public double distanceBetween(String labelOne, String labelTwo) {
+        Point foundOne = findPoint(labelOne);
+        Point foundTwo = findPoint(labelTwo);
+        if (foundOne != null && foundTwo != null)
+            return distanceBetween(foundOne.getX(), foundOne.getY(), foundTwo.getX(), foundTwo.getY());
+        return -1D;
+    }
+
+    public double distanceBetween(double x, double y) {
+        return distanceBetween(x, y, 0, 0);
     }
 
     public String getLabel() {
