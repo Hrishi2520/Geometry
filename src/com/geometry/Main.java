4f7953b669a836c2b45455aa60031a78b269c849
Ketan Jitendra Shinde <ks27543926@gmail.com>
2021-12-30 12:52:06 +0530

    Update Graph.java - Add pop overload methods - Add removePoint overload methods - Add distanceBetween overload methods - Restructure Graph.java

--- src/com/geometry/Main.java
+++ src/com/geometry/Main.java
@@ -13,10 +13,22 @@ public class Main {
         salary.addPoint(6, 25);
         Point point = salary.addPoint(-4, -12);
         Point bhavesh = salary.addPoint(-70, 100, "Bhavesh");
+        Point flash = salary.addPoint(-70, 100, "Flash");
         System.out.printf("findPoint(%s): %s\n", bhavesh, salary.findPoint(bhavesh));
         System.out.printf("findPoint(%.2f, %.2f): %s\n", -110D, 120D, salary.findPoint(-110D, 120D));
         System.out.printf("findPoint(%s): %s\n", "ketan", salary.findPoint("ketan"));
         System.out.printf("%s points: %s\n", salary.getLabel(), salary.getPoints());
+        System.out.printf("pop(%s): %b\n", "ketan", salary.pop("ketan"));
+        System.out.printf("findPoint(%s): %s\n", "ketan", salary.findPoint("ketan"));
+        System.out.printf("pop(%.2f, %.2f): %b\n", -110D, 120D, salary.pop(-110D, 120D));
+        System.out.printf("findPoint(%.2f, %.2f): %s\n", -110D, 120D, salary.findPoint(-110D, 120D));
+        System.out.printf("pop(%s): %b\n", bhavesh, salary.pop(bhavesh));
+        System.out.printf("findPoint(%s): %s\n", bhavesh, salary.findPoint(bhavesh));
+        System.out.printf("Distance between (%.2f, %.2f) and (%.2f, %.2f): %s\n",19D, 22D, 45D, 11D, salary.distanceBetween(19,22,45,11));
+        System.out.printf("pop(%s): %b\n", "shedge", salary.pop("shedge"));
+        System.out.printf("pop(%.2f, %.2f): %b\n", 25D, 20D, salary.pop(25,20));
+        System.out.printf("distance between two points %s and %s is: %.2f \n", "labelOne", "labelTwo", salary.distanceBetween("labelOne", "labelTwo"));
+        System.out.printf("distance between two points %s and origin: %.2f\n", "shedge", salary.distanceBetween("shedge"));
 
     }
 }
